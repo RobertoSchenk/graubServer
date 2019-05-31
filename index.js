259530de4da7ccb69f039b41d1a5e0ebfe24c76d
Roberto Duarte <robertoschenkd@gmail.com>
2019-05-30 21:16:02 -0300

    Cors and game server

--- index.js
+++ index.js
@@ -1,10 +1,86 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+var cors = require('cors');
+var express = require('express');
+var app = express();
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept-Type');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  next();
+})
+
+var server = require('http').Server(app);
+var io = require('socket.io').listen(server);
+
+var players = {};
+var star = {
+  x: Math.floor(Math.random() * 700) + 50,
+  y: Math.floor(Math.random() * 500) + 50
+};
+var scores = {
+  blue: 0,
+  red: 0
+};
+
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept-Type');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  next();
+})
+
+app.use(express.static(__dirname + '/public'));
+
+
+io.on('connection', function (socket) {
+  console.log('a user connected: ', socket.id);
+  // create a new player and add it to our players object
+  players[socket.id] = {
+    rotation: 0,
+    x: Math.floor(Math.random() * 700) + 50,
+    y: Math.floor(Math.random() * 500) + 50,
+    playerId: socket.id,
+    team: (Math.floor(Math.random() * 2) == 0) ? 'red' : 'blue'
+  };
+  // send the players object to the new player
+  socket.emit('currentPlayers', players);
+  // send the star object to the new player
+  socket.emit('starLocation', star);
+  // send the current scores
+  socket.emit('scoreUpdate', scores);
+  // update all other players of the new player
+  socket.broadcast.emit('newPlayer', players[socket.id]);
+
+  // when a player disconnects, remove them from our players object
+  socket.on('disconnect', function () {
+    console.log('user disconnected: ', socket.id);
+    delete players[socket.id];
+    // emit a message to all players to remove this player
+    io.emit('disconnect', socket.id);
+  });
+
+  // when a player moves, update the player data
+  socket.on('playerMovement', function (movementData) {
+    players[socket.id].x = movementData.x;
+    players[socket.id].y = movementData.y;
+    players[socket.id].rotation = movementData.rotation;
+    // emit a message to all players about the player that moved
+    socket.broadcast.emit('playerMoved', players[socket.id]);
+  });
+
+  socket.on('starCollected', function () {
+    if (players[socket.id].team === 'red') {
+      scores.red += 10;
+    } else {
+      scores.blue += 10;
+    }
+    star.x = Math.floor(Math.random() * 700) + 50;
+    star.y = Math.floor(Math.random() * 500) + 50;
+    io.emit('starLocation', star);
+    io.emit('scoreUpdate', scores);
+  });
+});
+
+server.listen(8081, function () {
+  console.log(`Listening on ${server.address().port}`);
+});
