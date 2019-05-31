38359be70571f14c7b3689260de4db7087dfc339
Roberto <robertoschenkd@gmail.com>
2019-05-31 00:29:00 -0300

    Some Tests

--- index.js
+++ index.js
@@ -26,21 +26,25 @@ io.on('connection', function (socket) {
     playerId: socket.id,
     team: (Math.floor(Math.random() * 2) == 0) ? 'red' : 'blue'
   };
-  // send the players object to the new player
+  
+  
   socket.broadcast.emit('currentPlayers', { hello: 'world' });
   socket.emit('currentPlayers', players);
-  // send the star object to the new player
+
+  
   socket.emit('starLocation', star);
-  // send the current scores
+
+  
   socket.emit('scoreUpdate', scores);
-  // update all other players of the new player
+  
+  
   socket.broadcast.emit('newPlayer', players[socket.id]);
 
-  // when a player disconnects, remove them from our players object
+
   socket.on('disconnect', function () {
     console.log('user disconnected: ', socket.id);
     delete players[socket.id];
-    // emit a message to all players to remove this player
+
     io.emit('disconnect', socket.id);
   });
 
@@ -59,15 +63,24 @@ io.on('connection', function (socket) {
     } else {
       scores.blue += 10;
     }
+
     star.x = Math.floor(Math.random() * 700) + 50;
     star.y = Math.floor(Math.random() * 500) + 50;
+
     io.emit('starLocation', star);
     io.emit('scoreUpdate', scores);
   });
 });
 
+function tick()
+{
+    return {
+      time : new Date().getSeconds
+    };
+}
+
 server.listen(process.env.PORT || 8081, function () {
   console.log(`Listening on ${server.address().port}`);
 
-  setInterval(() => io.emit('currentPlayers', new Date().toTimeString()), 1000);
+  setInterval(() => io.emit('tick', tick()), 100);
 });
