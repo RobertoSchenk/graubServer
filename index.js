232990aaabe412f46117dad43e1820b1de89775d
Roberto Duarte <robertoschenkd@gmail.com>
2019-05-30 22:05:29 -0300

    removed cors for testing

--- index.js
+++ index.js
@@ -22,12 +22,7 @@ var scores = {
   red: 0
 };
 
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept-Type');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  next();
-})
+
 
 app.use(express.static(__dirname + '/public'));
 
